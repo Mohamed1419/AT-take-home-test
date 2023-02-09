963d72d976d09298dc40c70267ade5c19554871c
Mohamed <mohamed.sheik98@outlook.com>
2023-02-09 12:28:19 +0000

    get api working

--- at-test/src/pages/HomePage/HomePage.jsx
+++ at-test/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 import './HomePage.css'
 
 function HomePage() {
+    let [samples, setSamples] = useState([])
+
+    useEffect(() => {
+         const getSamples = async () => {
+      try {
+        const res = await fetch('https://global.atdtravel.com/api/products?geo=en');
+        const resultsData = await res.json();
+        setSamples(resultsData)
+        console.log(resultsData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getSamples();
+  }, []);
+
+
   return (
-    <div>HomePage</div>
+    <div>
+      <ul>
+      {
+        samples ? (samples.data.map((post) => (<li key={post.id}>{post.title}</li>))) : <h1>loading</h1>
+      }
+      </ul>
+    </div>
   )
 }
 
