aa25374797d6c8fb14136ada64cca22c638135df
Mohamed <mohamed.sheik98@outlook.com>
2023-02-09 16:50:49 +0000

    search & re-search working

--- at-test/src/pages/ResultsPage/ResultsPage.jsx
+++ at-test/src/pages/ResultsPage/ResultsPage.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom';
 import './ResultsPage.css'
 
 function ResultsPage() {
+    let params = useParams();
+    let [results, setResults] = useState()
+    console.log(params.query);
+
+    useEffect(() => {
+      const getSamples = async () => {
+      try {
+        const res = await fetch(`https://global.atdtravel.com/api/products?geo=en&title=${params.query}`);
+        const resultsData = await res.json();
+        console.log(resultsData);
+        setResults(resultsData)
+        console.log(results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getSamples();
+  }, [params.query]);
+  //whenever there is a change in the params ie a new search has been made, the useEffect block is called again to fetch based on the new query
+
   return (
-    <div>ResultsPage</div>
+        <>
+      {results && Object.keys(results).length > 1 ? results.data.map((data)=>(<h2 key={data.id}>{data.title}</h2>)) : results && Object.keys(results).length === 1 ? <h2>{results.err_desc}</h2> : <h2>Loading...</h2>}
+        </>
   )
 }
 
